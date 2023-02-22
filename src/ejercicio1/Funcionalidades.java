42ba48b8c07ee4f3628d9271d067c4e7b3ea3e87
Txalo10 <gon.alejandrez@gmail.com>
2023-02-22 22:03:11 +0100

    Modificación 22.02

--- src/ejercicio1/Funcionalidades.java
+++ src/ejercicio1/Funcionalidades.java
@@ -2,6 +2,13 @@ package ejercicio1;
 
 public class Funcionalidades {
 
+	/*@author: Gonzalo Alejándrez Andión
+	 * @param palabra: define el dato, string en este caso, que se va a buscar 
+	 * @param arrayPalabras: define el array en el que se va a buscar el string
+	 * @return resultado: nos va a dar la posición dentro del array que ocupa el string que hemos buscado
+	 * @version: 22.02.2023
+	 */
+	
 	public static int buscarPalabra(String palabra, String[] arrayPalabras) {
 
 		int resultado = -1;
@@ -19,7 +26,12 @@ public class Funcionalidades {
 	}
 
 	
-	
+	/*@author: Gonzalo Alejándrez Andión
+	 * @param numero: define el dato, int en este caso, que se va a buscar 
+	 * @param arrayNumeros: define el array en el que se va a buscar el número
+	 * @return resultado: nos va a dar la posición dentro del array que ocupa el número que hemos buscado
+	 * @version: 22.02.2023
+	 */
 	
 	public static int buscarNumero(int numero, int[] arrayNumeros) {
 		int resultado = -1;
